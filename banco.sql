64886f8f680afbbb8a86e61fa03baf4e68f207ea
dms18 <83821159+dms18@users.noreply.github.com>
2023-12-06 17:27:52 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -1,8 +1,8 @@
-CREATE TABLE dados (
-    AlunoID int,
+CREATE TABLE sup_cliente (
+    ClienteID int,
     Nome varchar(50),
     Sobrenome varchar(50),
     Endereco varchar(150),
     Cidade varchar(50),
-    Host varchar(50)
+    email varchar(20)
 );
